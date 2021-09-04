405724655dc5419fec7e10376b627d2eee49dfaf
krishsam94 <krishsam94@gmail.com>
2021-09-04 11:33:50 +0530

    Test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  let [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <h1>Hello </h1>
+      <h1>Hello {count}</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
